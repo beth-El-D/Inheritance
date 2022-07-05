5ed572eaeab2d4aec34885bacc5d0cb4e5fa445f
beth-El-D <105913444+beth-El-D@users.noreply.github.com>
2022-07-05 23:13:22 +0900

    Circle and Rectangle classes inherit some properties from IShape1

---
+++ Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mnbv
+{
+
+    internal class Class1
+    {
+    }
+
+    abstract class IShape1
+    {
+        abstract public double calculateArea();
+        abstract public double calculatePerimeter();
+    }
+
+    class Circle : IShape1
+    {
+        public double radius { get; set; }
+        public static double PI = 3.14;
+        public double area { get; set; }
+        public double perimeter { get; set; }
+
+        public override double calculateArea()
+        {
+            area = PI * (radius * radius);
+            return area;
+        }
+
+        public override double calculatePerimeter()
+        {
+            perimeter = 2 * PI * radius;
+            return perimeter;
+        }
+    }
+
+    class rectangle : IShape1
+    {
+
+        public double length { get; set; }
+        public double width { get; set; }
+        public double area { get; set; }
+        public double perimeter { get; set; }
+
+
+        public override double calculateArea()
+        {
+            area = length * width;
+            return area;
+        }
+
+        public override double calculatePerimeter()
+        {
+            perimeter = 2 * (length + width);
+            return perimeter;
+        }
+    }
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using mnbv;
+
+Circle c = new Circle();
+rectangle r = new rectangle();
+
+int choice;
+char what;
+
+  do
+{
+
+    Console.WriteLine("\t1. Calculate Area of Circle");
+    Console.WriteLine("\t2. Calculate Perimeter of Circle");
+
+    Console.WriteLine("\t3. Calculate Area of Rectangle");
+
+    Console.WriteLine("\t4. Calculate Perimeter of Rectangle");
+   
+outer: { choice = int.Parse(Console.ReadLine()); }
+
+
+    if (choice==1)
+            {
+                Console.WriteLine("Enter radius: ");
+                c.radius = Convert.ToDouble(Console.ReadLine());
+                c.calculateArea();
+                Console.WriteLine("Area= " + c.area);
+            }
+          else if(choice==2)
+            {
+                Console.WriteLine("Enter radius: ");
+                c.radius = Convert.ToDouble(Console.ReadLine());
+                c.calculatePerimeter();
+                Console.WriteLine("Perimeter= " + c.perimeter);
+            }
+
+          else if(choice ==3)
+            {
+                Console.WriteLine("Enter length: ");
+                r.length = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter width: ");
+                r.width = Convert.ToDouble(Console.ReadLine());
+                r.calculateArea();
+                Console.WriteLine("Area= " + r.area);
+            }
+
+           else if(choice==4)
+            {
+                Console.WriteLine("Enter length: ");
+                r.length = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter width: ");
+                r.width = Convert.ToDouble(Console.ReadLine());
+                r.calculatePerimeter();
+                Console.WriteLine("Perimeter= " + r.perimeter);
+            }
+
+    else
+    {
+        Console.WriteLine("Please enter proper choice(1-4)");
+       goto outer;
+        
+    }
+    
+    Console.WriteLine("Do you want to continue:('Y' or 'N'");
+    what = Convert.ToChar(Console.ReadLine());
+   
+
+} while (what == 'Y');
+            
